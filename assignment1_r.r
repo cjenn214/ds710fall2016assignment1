262189db5c228c7bf26b71adeebee35e0ab1bc4c
cjenn214 <cjenn214@gmail.com>
2016-09-12 21:59:35 -0400

    R Portion Complete

---
+++ assignment1_r.r
@@ -0,0 +1,26 @@
+# R version 3.3.1 (2016-06-21) -- "Bug in Your Hair"
+#1.1
+2015^(1/3)
+#1.2
+abs(5.7-6.8)/.58
+#1.3
+a = 1:12
+a
+#1.4
+b = c(1,3,5,7,9,11)
+b
+#1.5
+c= seq(1,11, 2)
+c
+#1.6
+ln.a = log(a)
+ln.a
+#1.7
+sq.a = sqrt(a)
+sq.a
+#1.8
+?sd
+# ?sd pulls up the help window for standard deviation
+#1.9
+Name = "Clint"
+paste("my name is", Name)
